4eab36c059b8abcdb630b4289d27bf9bd317a7b9
Dagan Gan <daganboy@gmail.com>
2015-04-13 17:36:26 -0400

    PEP 8 compliance

--- main.py
+++ main.py
@@ -1,56 +1,55 @@
 #!/usr/bin/env python
 
-#import modules - STA includes Tweepy
+# import modules - STA includes Tweepy
 from SimpleTwitterAnalyzer import*
 import json
 import sys
 
 
-#Authenticate application and create API object
-sys.stdout.write("Authorizing application\r")
-auth=tweepy.OAuthHandler("9s9WDsekprNSQ51VFqyInPkdi", "B4LLWF91qLicznrmlGP7hgXToj85AJ1OUXdQOZP90vgDUaWDSP")
-api=tweepy.API(auth)
+# Authenticate application and create API object
+print("Authorizing application")
+auth = tweepy.OAuthHandler("9s9WDsekprNSQ51VFqyInPkdi", "B4LLWF91qLicznrmlGP7hgXToj85AJ1OUXdQOZP90vgDUaWDSP")
+api = tweepy.API(auth)
 
 
 # Collect tweets
-print("")
-if(sys.version_info[0]==2):
-	query=raw_input("Query>")
-	max_number_of_results=int(raw_input("Max Results>"))
+if sys.version_info[0] == 2:
+    query = raw_input("Query>")
+    max_number_of_results = int(raw_input("Max Results>"))
 else:
-	query=input("Query>")
-	max_number_of_results=int(input("Max Results>"))
-tweets=getTweets(api,query, max=max_number_of_results)# get 'max' results
+    query = input("Query>")
+    max_number_of_results = int(input("Max Results>"))
+tweets = get_tweets(api, query, max_results=max_number_of_results)  # get 'max' results
 
 
 # Collect tweet text and JSON data
 sys.stdout.write("Converting Data...          \r")
-tweet_texts=[]
-json_dump=""
-tweetcount=0;
+tweet_texts = []
+json_dump = ""
+tweetcount = 0
 for tweet in tweets:
     tweet_texts.append(tweet.text)
     json_dump += json.dumps(tweet._json)+"\n"
-    tweetcount+=1
+    tweetcount += 1
 
 
 # Get word counts
 sys.stdout.write("Doing Wordcount...          \r")
-wordcount=getWordCounts(getWords(tweet_texts,exclude=STOP_WORDS+TWITTER_WORDS+ALPHABET))
+wordcount=get_word_counts(get_words(tweet_texts, exclude=STOP_WORDS+TWITTER_WORDS+ALPHABET))
 
 
 # Export JSON
 sys.stdout.write("Exporting Data...          \r")
-json_file=open("twitter_dump("+query+").json", "w")
+json_file = open("twitter_dump("+query+").json", "w")
 json_file.write(json_dump)
-json_file.close();
+json_file.close()
 
 
 # Export word count
-wc_text=""
+wc_text = ""
 for i in wordcount:
     wc_text += i[0]+":"+str(i[1])+"\n"
-wc_file=open("twitter_dump("+query+")_wordcount.txt","w")
+wc_file = open("twitter_dump("+query+")_wordcount.txt","w")
 wc_file.write(wc_text)
 wc_file.close()
 
