aef1289fb504e69380c4fe68b4ea02ea06a4937b
Shelley Martinez <martsha@Shelley-Martinezs-MacBook-Pro.local>
2015-05-17 09:26:26 -0400

    Changed wordcount output from txt to csv format. Other minor changes

--- TweetAnalyzer.py
+++ TweetAnalyzer.py
@@ -6,16 +6,18 @@
 print("Setting up...")
 from tweetalysis import*
 import json
-import sys
+import os
 import time
 
 # Authenticate application and create API object
-auth = tweepy.OAuthHandler(open("keys/consumer_key", "r").read(), open("keys/consumer_secret", "r").read())
+key=open("keys/consumer_key", "r").read().replace("\n", "").replace(" ", "")
+secret=open("keys/consumer_secret", "r").read().replace("\n", "").replace(" ", "")
+auth =tweepy.OAuthHandler(key, secret)
 api = tweepy.API(auth)
 
 
 # Collect tweets
-if sys.version_info[0] == 2:
+if os.sys.version_info[0] == 2:
     query = raw_input("\nQuery>")
     max_number_of_results = int(raw_input("Max Results>"))
 else:
@@ -25,7 +27,7 @@ print("")
 tweets = get_tweets(api, query, max_results=max_number_of_results)  # get 'max' results
 
 # Converting data
-sys.stdout.write("Converting Data...          \r")
+os.sys.stdout.write("Converting Data...          \r")
 tweet_texts = []
 json_dump = ""
 tweetcount = 0
@@ -36,22 +38,25 @@ for tweet in tweets:
 
 
 # Get word counts
-sys.stdout.write("Performing Wordcount...          \r")
+os.sys.stdout.write("Performing Wordcount...          \r")
 wordcount = get_word_counts(get_words(tweet_texts, exclude=STOP_WORDS+TWITTER_WORDS+ALPHABET))
 
 
 # Export JSON
+
 sys.stdout.write("Exporting Data...          \r")
-json_file = open("twitter_dump("+query+").json", "w")
+if not os.path.exists("twitter_data"):
+    os.makedirs("twitter_data")
+json_file = open("twitter_data/"+query+"_dump.json", "w")
 json_file.write(json_dump)
 json_file.close()
 
 
 # Export word count
-wc_text = ""
+wc_text = "Word,Count\n"
 for i in wordcount:
-    wc_text += i[1]+":"+str(i[0])+"\n"
-wc_file = open("twitter_dump("+query+")_wordcount.txt","w")
+    wc_text += i[1]+","+str(i[0])+"\n"
+wc_file = open("twitter_data/"+query+"_wordcount.csv","w")
 wc_file.write(wc_text)
 wc_file.close()
 
